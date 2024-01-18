1b0df88d855064fb2d8a45d66e14ec5c0d5a77c9
米良丰 <2622891570@qq.com>
2024-01-18 18:49:11 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view v-wechat-title="$route.meta.title"></router-view>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld
+    }
+  }
+</script>
+
+<style>
+  html {
+    overflow-y: hidden;
+  }
+</style>
